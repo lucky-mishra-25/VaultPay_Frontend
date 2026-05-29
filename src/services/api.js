fcd1e59914ae8fa522f02c8b0a2e96f210fe7b9e
Lucky Mishra <luckymishra4097gmail.com>
2026-05-29 18:04:49 +0530

    fixed api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL,
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+  const token =
+    localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    req.headers.Authorization =
+      `Bearer ${token}`;
   }
 
   return req;
