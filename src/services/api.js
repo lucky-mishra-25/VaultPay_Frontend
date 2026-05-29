5115e092c753fab9aa07cbd0c6241881c6ff900d
Lucky Mishra <luckymishra4097gmail.com>
2026-05-29 18:15:51 +0530

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    process.env.REACT_APP_API_URL,
+    "https://vaultpay-backend-oi8j.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
